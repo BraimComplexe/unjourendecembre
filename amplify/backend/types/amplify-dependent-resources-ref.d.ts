dac8785fe07986a749cdee8876e08cee2b55acf2
corbe_home <maxime.pl.corbe@wanadoo.fr>
2022-05-19 17:47:25 +0200

    supprime fichier inutile

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "unjourendecembre": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
